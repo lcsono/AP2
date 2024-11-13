58b9b3f988fb88b0cea688c5e45f3476a1968e28
Miguel-ectil <ectilmiguel@gmail.com>
2024-11-13 09:35:38 -0300

    [UPD] Atualizadno o arquiv db_config pois era lá que precisava mexerpara retirar os logs do banco ao usa-lo

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Conexão com o banco de dados SQLite
 DATABASE_URL = "sqlite:///gerenciamento_tarefas.db"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
